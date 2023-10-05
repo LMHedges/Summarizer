637dd7770a7004c0d278412375333efdaa08c3b8
Louis Hedges <hedges.louis0@gmail.com>
2023-10-05 11:59:45 +0100

    push 1

--- Summarizer/Summarizer.cpp
+++ Summarizer/Summarizer.cpp
@@ -10,8 +10,17 @@ class WordFreqPair {
 public:
     string word;
     int count;
+    WordFreqPair(string inputWord) {
+        word = inputWord;
+        count = 1;
+    }
 };
 string text;
+string wordHolder;
+string wordcounter;
+string spaces;
+int pointer = 0;
+int spaceNum;
 int numberOfWords = 0;
 int numberOfSentences = 0;
 string filename = "input.txt";
@@ -25,14 +34,48 @@ int main()
             if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
                 numberOfSentences++;
                 numberOfWords++;
+                for (pointer; pointer <= i; pointer++) {
+                    wordHolder = wordHolder + text[pointer];
+                }
+                cout << wordHolder << endl;
+                pointer = i + 1;
+                wordHolder.clear();
+                for (auto savedWord : wordlist) {
+                    if (savedWord.word == wordHolder) {
+                        savedWord.count++;
+                    }
+                }
+                
+                //WordFreqPair obj(wordHolder);
+                //wordlist.push_back(obj);
             }
             else if(text[i] == ' ' || text[i] == ',') {
                 numberOfWords++;
+                for (pointer; pointer <= i; pointer++) {
+                    wordHolder = wordHolder + text[pointer];
+                }
+                cout << wordHolder << endl;
+                pointer = i + 1;
+                wordHolder.clear();
+                for (auto savedWord : wordlist) {
+                    if (savedWord.word == wordHolder) {
+                        savedWord.count++;
+                    }
+                }
+
             }
+
         }
     }
 
     cout << "number of sentences is " << numberOfSentences << endl;
     cout << "number of words is " << numberOfWords << endl;
+    for (auto savedWord : wordlist) {
+        spaceNum = 8 - savedWord.word.length();
+        for (int i = 0; i <= spaceNum; i++) {
+            spaces = spaces + ' ';
+        }
+        cout << savedWord.word << spaces << savedWord.count << endl;
+    }
     fileReader.close();
 }
